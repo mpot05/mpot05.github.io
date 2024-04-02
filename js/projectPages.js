d9ea2d84be0d580000461e9db7fb475e576f60f7
Michael Potter <mpotter0905@gmail.com>
2024-04-02 09:56:16 -0400

    Index got nuked, put projects into one changing html page

--- js/projectPages.js
+++ js/projectPages.js
@@ -1,13 +1,50 @@
 
 
 function loadCalc() {
-    localStorage.setItem("SrcLink", "https://github.com/mpot05/first-calculator-app")
-    localStorage.setItem("SrcImg", "/images/calc_image.png")
-    localStorage.setItem("Title", "Calculator App")
+    sessionStorage.setItem("SrcLink", "https://github.com/mpot05/first-calculator-app")
+    sessionStorage.setItem("SrcImg", "/images/calc_image.png")
+    sessionStorage.setItem("Title", "Calculator App")
+    sessionStorage.setItem("Proj_Desc", "This project was one of my very early on SwiftUI apps that I made. It was for an in class assignment that only required two numbers to multiply. I wanted to do more with it because I thought there was a lot more possible. I ended up making an entire calculator. Though, because I was very new to programming, it has plenty of bugs. I had only been programming for about a month and a half at this point.")
+}
+
+function loadHack() {
+    sessionStorage.setItem("SrcLink", "https://github.com/mpot05/First-Hackathon")
+    sessionStorage.setItem("SrcImg", "/images/hackathon-image.png")
+    sessionStorage.setItem("Title", "First Hackathon")
+    sessionStorage.setItem("Proj_Desc", "This is my first Hackathon Project from an in class version of a Hackathon. We worked together in groups and were given default pacman assets to build whatever we wanted.")
+}
+
+function loadSpritekit() {
+    sessionStorage.setItem("SrcLink", "https://github.com/mpot05/FirstSpriteKit")
+    sessionStorage.setItem("SrcImg", "/images/spritekitimg.jpeg")
+    sessionStorage.setItem("Title", "Spritekit App")
+    sessionStorage.setItem("Proj_Desc", "Spritekit App")
+}
+
+function loadPortfolio() {
+    sessionStorage.setItem("SrcLink", "https://github.com/mpot05/MPortfolio")
+    sessionStorage.setItem("SrcImg", "/images/Portfolio.png")
+    sessionStorage.setItem("Title", "Portfolio App")
+    sessionStorage.setItem("Proj_Desc", "This is my portfolio app made in SwiftUI. I made this project in my 2nd year of programming to show what I'd made so far in SwiftUI. It's for the most part just a combination of my best apps from my 1st year.")
+}
+
+function loadArduino() {
+    sessionStorage.setItem("SrcLink", "https://github.com/mpot05/arduinoClicker")
+    sessionStorage.setItem("SrcImg", "/images/arduino.png")
+    sessionStorage.setItem("Title", "Arduino Clicker")
+    sessionStorage.setItem("Proj_Desc", "This is one of my first experiences with coding with physical things. So of course, my first idea for a project was make a physical cookie clicker because why not. My objective is to make a cookie clicker in every format and language I work with.")
+}
+
+function loadMicrobit() {
+    sessionStorage.setItem("SrcLink", "https://makecode.microbit.org/S13099-88584-78541-08255")
+    sessionStorage.setItem("SrcImg", "/images/microbit.png")
+    sessionStorage.setItem("Title", "Microbit Clicker")
+    sessionStorage.setItem("Proj_Desc", "This is my 2nd physical programming project. I had a much harder time working with microbits than with arduino. This was mostly due to the lack of tutorial videos for it in comparison to an arduino board. Anyways, it was a bit nicer to program since microbits allow you to use block code which was simple but also only allowed a simpler implementation.")
 }
 
 window.onload = function() {
-    document.getElementById("Image").src = localStorage.getItem("SrcImg")
-    document.getElementById("Link").innerText = localStorage.getItem("SrcLink")
-    document.getElementById("title").innerHTML = localStorage.getItem("Title")
+    document.getElementById("Image").src = sessionStorage.getItem("SrcImg")
+    document.getElementById("Link").href = sessionStorage.getItem("SrcLink")
+    document.getElementById("Title").innerText = sessionStorage.getItem("Title")
+    document.getElementById("Proj_Desc").innerText = sessionStorage.getItem("Proj_Desc")
 }

79d5893defd725b6d5eb9ecb93121d61b8b77310
Michael Potter <mpotter0905@gmail.com>
2024-04-09 09:29:17 -0400

    plusMinus finally works, added navbar to calc page, updated navbar, might add more styling to calc page eventually

--- js/calc.js
+++ js/calc.js
@@ -6,34 +6,31 @@ let hasDecimal = false;
 let operate = false;
 let state = 0;
 
-
 function calculate() {
     if (operate == true) {
         switch (state) {
             case 1:
-                document.getElementById("displaynum").innerHTML = (Number(displaynum) + Number(displaynum2));
+                setHTML(Number(displaynum) + Number(displaynum2));
                 Clear()
                 break;
             case 2:
-                document.getElementById("displaynum").innerHTML = (Number(displaynum) - Number(displaynum2));
+                setHTML(Number(displaynum) - Number(displaynum2));
                 Clear()
                 break;
             case 3:
-                document.getElementById("displaynum").innerHTML = (Number(displaynum) * Number(displaynum2));
+                setHTML(Number(displaynum) * Number(displaynum2));
                 Clear()
                 break;
             case 4:
-                document.getElementById("displaynum").innerHTML = (Number(displaynum) / Number(displaynum2));
+                setHTML(Number(displaynum) / Number(displaynum2));
                 Clear()
                 break;
             default:
-                document.getElementById("displaynum").innerHTML = "You broke it"
+                setHTML("You broke it")
                 break;
         }
     }
 }
-
-
 
 function Clear() {
     num1 = null;
@@ -51,43 +48,89 @@ function ClearAll() {
     displaynum2 = null;
     hasDecimal = false;
     operate = false;
-    document.getElementById("displaynum").innerHTML = "0";
+    setHTML("0");
+}
+
+function softReset(num) {
+    operate = true;
+    hasDecimal = false;
+    state = num;
+    displaynum2 = null;
+    setHTML("0");
 }
 
 function percentSign() {
     if (operate == false) {
         checkNull(1);
         displaynum = Number(displaynum) / 100;
-        document.getElementById("displaynum").innerHTML = displaynum;
+        setHTML(displaynum);
     }
     else {
         checkNull(2);
         displaynum2 = Number(displaynum2) / 100;
-        document.getElementById("displaynum").innerHTML = displaynum2;
+        setHTML(displaynum2);
     }
 }
 
 function plusMinus() {
-    let thing = "-"
-    checkNull(1);
-    checkNull(2);
+    let thing = "-";
     if (operate == false) {
+        checkNull(1);
         switch (Math.sign(Number(displaynum))) {
             case 1:
-                
+                thing += displaynum;
+                displaynum = thing;
+                setHTML(displaynum);
+                thing = "-"
                 break;
             case -1:
-
+                displaynum = String(displaynum).substring(1);
+                setHTML(displaynum);
                 break;
             case 0:
-
+                if (doesIncludeHTML("-")) {
+                    displaynum = String(displaynum).substring(1);
+                    setHTML(displaynum);
+                }
+                else {
+                    thing += displaynum;
+                    displaynum = thing;
+                    setHTML(displaynum);
+                    thing = "-"
+                }
                 break;
             default:
                 break;
         }
     }
     else {
-
+        checkNull(2);
+        switch (Math.sign(Number(displaynum2))) {
+            case 1:
+                thing += displaynum2;
+                displaynum2 = thing;
+                setHTML(displaynum2)
+                thing = "-"
+                break;
+            case -1:
+                displaynum2 = String(displaynum2).substring(1);
+                setHTML(displaynum2)
+                break;
+            case 0:
+                if (String(displaynum2).includes("-")) {
+                    displaynum2 = String(displaynum2).substring(1);
+                    setHTML(displaynum2)
+                }
+                else {
+                    thing += displaynum2;
+                    displaynum2 = thing;
+                    setHTML(displaynum2)
+                    thing = "-"
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
 
@@ -98,83 +141,113 @@ function decimal() {
             checkNull(1);
             displaynum = String(displaynum);
             displaynum = displaynum.concat(".");
-            document.getElementById("displaynum").innerHTML = displaynum;
+            setHTML(displaynum);
         }
         else {
             checkNull(2);
             displaynum2 = String(displaynum2);
             displaynum2 = displaynum2.concat(".");
-            document.getElementById("displaynum").innerHTML = displaynum2;
+            setHTML(displaynum2);
         }
         hasDecimal = true
     }
 }
 
 function addition() {
-    operate = true;
-    hasDecimal = false;
-    state = 1;
-    document.getElementById("displaynum").innerHTML = "0";
+    softReset(1);
 }
 
 function subtraction() {
-    operate = true;
-    hasDecimal = false;
-    state = 2;
-    document.getElementById("displaynum").innerHTML = "0";
+    softReset(2);
 }
 
 function multiplication() {
-    operate = true;
-    hasDecimal = false;
-    state = 3;
-    document.getElementById("displaynum").innerHTML = "0";
+    softReset(3);
 }
 
 function division() {
-    operate = true;
-    hasDecimal = false;
-    state = 4;
-    document.getElementById("displaynum").innerHTML = "0";
+    softReset(4);
 }
 
 function numer(num) {
+    thing = "";
     if (displaynum == null) {
         displaynum = num;
-        document.getElementById("displaynum").innerHTML = displaynum;
+        setHTML(displaynum);
     }
-    else if (operate == true) {
+    else if (operate) {
         if (displaynum2 == null) {
             displaynum2 = num;
-            document.getElementById("displaynum").innerHTML = displaynum2
+            setHTML(displaynum2);
+        }
+        else if (doesIncludeHTML(".")) {
+            displaynum2 = String(displaynum2);
+            displaynum2 = displaynum2.concat(String(num));
+            setHTML(displaynum2);
+        }
+        else if (document.getElementById("displaynum").innerHTML == "-0") {
+            displaynum2 = "-" + String(num);
+            setHTML(displaynum2)
+        }
+        else if (displaynum2 == 0) {
+            displaynum2 = num;
+            setHTML(displaynum2)
         }
         else {
             displaynum2 = String(displaynum2);
             displaynum2 = displaynum2.concat(String(num));
-            document.getElementById("displaynum").innerHTML = displaynum2;
+            setHTML(displaynum2);
         }
     }
-    else if (displaynum != null) {
-        displaynum = String(displaynum);
-        displaynum = displaynum.concat(String(num));
-        document.getElementById("displaynum").innerHTML = displaynum;
+    else {
+        if (document.getElementById("displaynum").innerHTML == "-0") {
+            displaynum = "-" + String(num);
+            setHTML(displaynum);
+        }
+        else if (doesIncludeHTML(".")) {
+            displaynum = String(displaynum);
+            displaynum = displaynum.concat(String(num));
+            setHTML(displaynum);
+        }
+        else if (displaynum == 0) {
+            displaynum = num;
+            setHTML(displaynum);
+        }
+        else {
+            displaynum = String(displaynum);
+            displaynum = displaynum.concat(String(num));
+            setHTML(displaynum);
+        }   
     }
-    
 }
 
 function checkNull(num) {
     switch (num) {
         case 1:
             if (displaynum == null) {
-                displaynum = 0
+                displaynum = 0;
             }
             break;
         case 2:
             if (displaynum2 == null) {
-                displaynum2 = 0
+                displaynum2 = 0;
             }
             break;
         default:
             break;
     }
+}
+
+function setHTML(str) {
+    document.getElementById("displaynum").innerHTML = str;
+}
+
+function doesIncludeHTML(str) {
+    if (document.getElementById("displaynum").innerHTML.includes(str)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
 }
